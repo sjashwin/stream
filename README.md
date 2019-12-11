f49d510540e345775191be303cabb8f77823c6c9
Ashwin Balasubramanian <balathinnappan@gmail.com>
2019-12-10 16:36:35 -0800

    Stream Processing API.
    This is the first commit that has basic streaming api functionalities
    for Integer.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Stream Processing API for Golang
+This repository is a stream processing api for golang. The api uses channels to communicate between
+stream. This api is meant to be light weight to provide developers with basic stream solutions.
---
+++ stream.go
@@ -0,0 +1,29 @@
+package stream
+
+// Stream interface
+type Stream interface {
+	Convert() <-chan int
+
+	Sum() int
+	Average() int
+	Min() int
+	Max() int
+}
+
+// Integer type pipeline
+type Integer struct {
+	Values []int
+	len    int
+}
+
+// Convert given int slice to a stream
+func (i *Integer) Convert() <-chan int {
+	var out chan int = make(chan int)
+	go func() {
+		for _, num := range i.Values {
+			out <- num
+		}
+		close(out)
+	}()
+	return out
+}
