31ecb8e9cc39866c5b51a1b180f7eafe45a8ec36
Ashwin Balasubramanian <balathinnappan@gmail.com>
2019-12-15 08:33:29 -0800

    Add Operation: Operator enabled
    Completed Jobs:
    
    	- Add Operator
    
    	- padding for the given slices
    
    Changed:
    
    	- Convert concrete return type to interface in Stream interface struct

--- computation.go
+++ computation.go
@@ -3,28 +3,48 @@ package stream
 import "sort"
 
 // Sum all the values in a given stream
-func (i *Integer) Sum() int {
+func (i *Integer) Sum() interface{} {
 	var sum int
 	for num := range i.Convert() {
-		sum += num
+		sum += num.(int)
 	}
 	return sum
 }
 
+// Len of an Integer stream type
+func (i *Integer) Len() int {
+	return len(*i)
+}
+
+// Len of a Float stream type
+func (f *Float) Len() int {
+	return len(*f)
+}
+
+// Val stored in a stream
+func (i *Integer) Val() interface{} {
+	return *i
+}
+
+// Val stored in a stream
+func (f *Float) Val() interface{} {
+	return *f
+}
+
 // Average of all values in a given stream
-func (i *Integer) Average() int {
+func (i *Integer) Average() interface{} {
 	var sum int
 	for num := range i.Convert() {
-		sum += num
+		sum += num.(int)
 	}
-	return sum / len(i.Values)
+	return sum / i.Len()
 }
 
 // Min of all values in a given stream
-func (i *Integer) Min() int {
-	var min int = <-i.Convert()
+func (i *Integer) Min() interface{} {
+	min := <-i.Convert()
 	for num := range i.Convert() {
-		if num < min {
+		if num.(int) < min.(int) {
 			min = num
 		}
 	}
@@ -32,10 +52,10 @@ func (i *Integer) Min() int {
 }
 
 //Max of all the values in a given stream
-func (i *Integer) Max() int {
-	var max int = <-i.Convert()
+func (i *Integer) Max() interface{} {
+	max := <-i.Convert()
 	for num := range i.Convert() {
-		if num > max {
+		if num.(int) > max.(int) {
 			max = num
 		}
 	}
@@ -44,38 +64,38 @@ func (i *Integer) Max() int {
 
 // Filter values from a given integer slice
 func (i *Integer) Filter(f func(int) bool) {
-	var buffer []int = make([]int, 0, len(i.Values))
+	var buffer Integer = make(Integer, 0, i.Len())
 	for num := range i.Convert() {
-		if f(num) {
-			buffer = append(buffer, num)
+		if f(num.(int)) {
+			buffer = append(buffer, num.(int))
 		}
 	}
-	i.Values = buffer
+	i = &buffer
 }
 
 // Sum of all values in a given float slice
-func (f *Float) Sum() float64 {
+func (f *Float) Sum() interface{} {
 	var sum float64
 	for num := range f.Convert() {
-		sum += num
+		sum += num.(float64)
 	}
 	return sum
 }
 
 // Average of all values in a given float slice
-func (f *Float) Average() float64 {
+func (f *Float) Average() interface{} {
 	var sum float64
 	for num := range f.Convert() {
-		sum += num
+		sum += num.(float64)
 	}
 	return sum
 }
 
 // Min of all values in a given float slice
-func (f *Float) Min() float64 {
-	var min float64 = <-f.Convert()
+func (f *Float) Min() interface{} {
+	var min interface{} = <-f.Convert()
 	for num := range f.Convert() {
-		if num < min {
+		if num.(float64) < min.(float64) {
 			min = num
 		}
 	}
@@ -83,10 +103,10 @@ func (f *Float) Min() float64 {
 }
 
 // Max of all values in a given float slice
-func (f *Float) Max() float64 {
-	var max float64 = <-f.Convert()
+func (f *Float) Max() interface{} {
+	var max interface{} = <-f.Convert()
 	for num := range f.Convert() {
-		if num > max {
+		if num.(float64) > max.(float64) {
 			max = num
 		}
 	}
@@ -95,37 +115,37 @@ func (f *Float) Max() float64 {
 
 // Filter values from a given float slice
 func (f *Float) Filter(v func(float64) bool) {
-	var buffer []float64 = make([]float64, 0, len(f.Values))
+	var buffer Float = make(Float, 0, f.Len())
 	for num := range f.Convert() {
-		if v(num) {
-			buffer = append(buffer, num)
+		if v(num.(float64)) {
+			buffer = append(buffer, num.(float64))
 		}
 	}
-	f.Values = buffer
+	f = &buffer
 }
 
 // Map the values of a Int slice to a given function
 func (i *Integer) Map(f func(int) int) {
 	var count int
 	for num := range i.Convert() {
-		i.Values[count] = f(num)
+		(*i)[count] = f(num.(int))
 		count++
 	}
 }
 
 // Map the values of a float slice to a give function
-func (f Float) Map(v func(float64) float64) {
+func (f *Float) Map(v func(float64) float64) {
 	var count int
 	for num := range f.Convert() {
-		f.Values[count] = v(num)
+		(*f)[count] = v(num.(float64))
 		count++
 	}
 }
 
 // Distinct all values are distinct in a given stream of Ints
-func (i *Integer) Distinct() bool {
+func (i *Integer) Distinct() interface{} {
 	var mode map[int]bool = make(map[int]bool)
-	for _, v := range i.Values {
+	for _, v := range *i {
 		if _, ok := mode[v]; ok {
 			return false
 		}
@@ -135,36 +155,36 @@ func (i *Integer) Distinct() bool {
 }
 
 // Peek the elements from the stream
-func (i *Integer) Peek() int {
-	return i.Values[0]
+func (i *Integer) Peek() interface{} {
+	return (*i)[0]
 }
 
 // Sort the given stream
 func (i *Integer) Sort() {
-	sort.Ints(i.Values)
+	sort.Ints(*i)
 }
 
 // ForEach does an operation on each element in the stream
 func (i *Integer) ForEach(f func(int)) {
 	for num := range i.Convert() {
-		f(num)
+		f(num.(int))
 	}
 }
 
 // ToSlice convert a stream to an Integer slice
 func (i *Integer) ToSlice() []int {
-	return i.Values
+	return *i
 }
 
 // Skip the number of elements in the stream
 func (i *Integer) Skip(num int) {
-	i.Values = i.Values[num:]
+	*i = (*i)[num:]
 }
 
 // Distinct is true if all values in the stream are unique
-func (f *Float) Distinct() bool {
+func (f *Float) Distinct() interface{} {
 	var mode map[float64]bool = make(map[float64]bool)
-	for _, v := range f.Values {
+	for _, v := range *f {
 		if _, ok := mode[v]; ok {
 			return false
 		}
@@ -175,22 +195,22 @@ func (f *Float) Distinct() bool {
 
 //Peek the elements from the stream
 func (f *Float) Peek() float64 {
-	return f.Values[0]
+	return (*f)[0]
 }
 
 // ForEach does an operation on each element in the stream
 func (f *Float) ForEach(v func(float64)) {
 	for num := range f.Convert() {
-		v(num)
+		v(num.(float64))
 	}
 }
 
 // ToSlice converts a stream to its equivalent interface
-func (f Float) ToSlice() []float64 {
-	return f.Values
+func (f *Float) ToSlice() []float64 {
+	return *f
 }
 
 // Skip the first n values in a given stream
 func (f *Float) Skip(num int) {
-	f.Values = f.Values[num:]
+	*f = (*f)[num:]
 }
--- stream.go
+++ stream.go
@@ -1,5 +1,10 @@
 package stream
 
+import (
+	"errors"
+	"reflect"
+)
+
 const (
 	//Version of this package
 	Version = "0.1.0"
@@ -7,31 +12,27 @@ const (
 
 // Stream interface
 type Stream interface {
-	Convert() <-chan int
+	Convert() <-chan interface{}
 
-	Sum() int
-	Average() int
-	Min() int
-	Max() int
+	Sum() interface{}
+	Average() interface{}
+	Min() interface{}
+	Max() interface{}
+	Len() int
+	Val() interface{}
 }
 
 // Integer type stream
-type Integer struct {
-	Values []int
-	len    int
-}
+type Integer []int
 
 // Float type stream
-type Float struct {
-	Values []float64
-	Len    int
-}
+type Float []float64
 
 // Convert given int slice to a stream
-func (i *Integer) Convert() <-chan int {
-	var out chan int = make(chan int)
+func (i *Integer) Convert() <-chan interface{} {
+	var out chan interface{} = make(chan interface{})
 	go func() {
-		for _, num := range i.Values {
+		for _, num := range *i {
 			out <- num
 		}
 		close(out)
@@ -40,13 +41,80 @@ func (i *Integer) Convert() <-chan int {
 }
 
 // Convert a given float slice to a stream
-func (f *Float) Convert() <-chan float64 {
-	var out chan float64 = make(chan float64)
+func (f *Float) Convert() <-chan interface{} {
+	var out chan interface{} = make(chan interface{})
 	go func() {
-		for _, num := range f.Values {
+		for _, num := range *f {
 			out <- num
 		}
 		close(out)
 	}()
 	return out
+}
+
+// Add two stream objects
+func Add(s1, s2 Stream) (Stream, error) {
+	var output Stream
+	if reflect.TypeOf(s1) != reflect.TypeOf(s2) {
+		return nil, errors.New("error: cannnot do add operation on distinct type")
+	}
+
+	switch s1.(type) {
+	case *Integer:
+		output = addInteger(s1.(*Integer), s2.(*Integer))
+	case *Float:
+		output = addFloat(s1.(*Float), s2.(*Float))
+	default:
+		return nil, errors.New("error: unsupported type")
+	}
+	return output, nil
+}
+
+func addInteger(num1, num2 *Integer) Stream {
+	if num1.Len() != num2.Len() {
+		padding(num1, num2)
+	}
+	var output []int = make([]int, num1.Len())
+	for i := 0; i < num1.Len(); i++ {
+		output[i] = (*num1)[i] + (*num2)[i]
+	}
+	var out Integer = output
+	return &out
+}
+
+func addFloat(num1, num2 *Float) Stream {
+	if num1.Len() != num2.Len() {
+		padding(num1, num2)
+	}
+	var output []float64 = make([]float64, num1.Len())
+	for i := 0; i < num1.Len(); i++ {
+		output[i] = (*num1)[i] + (*num2)[i]
+	}
+	var out Float = output
+	return &out
+}
+
+func padding(num1, num2 Stream) error {
+	var temp interface{}
+	switch num1.(type) {
+	case *Integer:
+		if num1.Len() > num2.Len() {
+			temp = make([]int, num1.Len()-num2.Len())
+			*num2.(*Integer) = append(*num2.(*Integer), temp.([]int)...)
+		} else {
+			temp = make([]int, num1.Len()-num2.Len())
+			*num1.(*Integer) = append(*num1.(*Integer), temp.([]int)...)
+		}
+	case *Float:
+		if num1.Len() > num2.Len() {
+			temp = make([]float64, num1.Len()-num2.Len())
+			*num2.(*Float) = append(*num2.(*Float), temp.([]float64)...)
+		} else {
+			temp = make([]float64, num1.Len()-num2.Len())
+			*num1.(*Float) = append(*num1.(*Float), temp.([]float64)...)
+		}
+	default:
+		return errors.New("error: unsupported type")
+	}
+	return nil
 }
